453c4d0b6493028d04a2ac43e8de883aead59c40
aseelaus <adrian.seelaus@gmx.ch>
2022-07-01 10:29:48 +0200

    database implementation in app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 '''CheckThis app'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from models.TaskDefinition import TaskDefinition_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+class TaskDefinition(db.Model):
+    '''Class TaskDefinition'''
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return f"TaskDefinition('{self.id}', '{self.title}'"
 
 #Provide some very basic test URL
 @app.route('/')
