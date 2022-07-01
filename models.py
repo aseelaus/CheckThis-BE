8c4677a31d2e3f9e9fd7fc116f3f5868e2f4dc96
aseelaus <adrian.seelaus@gmx.ch>
2022-07-01 11:28:01 +0200

    moving models into models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+'''Models'''
+
+from app import db
+
+class TaskDefinition(db.Model):
+    '''Class TaskDefinition'''
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return f"TaskDefinition('{self.id}', '{self.title}'"
